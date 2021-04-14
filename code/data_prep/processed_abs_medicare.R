c13c4bc3ff23f5fd0ccf189017ba0ee24ec8ac8d
Xilin Chen <xilinc@med.umich.edu>
2021-04-14 11:59:05 -0400

    cohort prep and diagram

--- code/data_prep/processed_abs_medicare.R
+++ code/data_prep/processed_abs_medicare.R
@@ -1,3 +1,5 @@
+# combine processed ABS data (redefined NA recertification) to medicare data
+
 library(dplyr)
 library(data.table)
 library(brms)
@@ -7,49 +9,56 @@ source("code/functions/prep_medicare_data.R")
 
 
 # read data ---------------------------------------------------------------
-abs = fread("data/processed_abs/defined_gs1_yoe17_grandpa2000.csv")
+# abs data was from abs_data_explore.rmd results
+# redefined NA as "no record, failed"
+# abs = fread("data/processed_abs/defined_gs1_yoe17_grandpa2000.csv")
+abs = fread("data/processed_abs/defined_gs2_grandpa1976.csv")
 medicare = fread("/Volumes/George_Surgeon_Projects/standardized_medicare_data_using_R/analysis_ready_data/analytic_selected_vars_5_procedure_upto2017.csv")
+
+# abs %>% tidyext::cat_by(ReCeverPassed)
 
 medicare_abs = inner_join(medicare %>% mutate(npi = as.integer(id_physician_npi)),
                           abs, by = "npi")
 
-abs %>% 
-  tidyext::cat_by(ReCeverPassed)
-
+# how many surgeons from abs were matched with Medicare cases
 medicare_abs %>% 
   distinct(npi, ReCeverPassed) %>% 
   tidyext::cat_by(ReCeverPassed)
 
 
 medicare_abs_model_ready = prep_data_for_model(medicare_abs) %>% 
-  mutate(ce_cert_status = case_when(ReCeverPassed == 0 ~ "failed",
+  mutate(re_cert_status = case_when(ReCeverPassed == 0 ~ "failed",
                                     ReCeverPassed == 1 ~ "passed",
-                                    ReCeverPassed == "no record" ~ "no_record"),
-         ce_cert_status = factor(ce_cert_status, levels = c("failed", "passed", "no_record")),
+                                    ReCeverPassed == "no record, failed" ~ "no record, failed"),
+         re_cert_status = factor(re_cert_status, levels = c("failed", "passed", "no record, failed")),
+         re_cert_bin = ifelse(re_cert_status == "passed", "passed", "no_pass_or_NA"),
+         
          n_attempts_recert = ifelse(nAttemptsReCert >=3, "≥3", nAttemptsReCert),
          n_attempts_recert = factor(n_attempts_recert, levels = c("1", "2", "≥3")))
 
 save(medicare_abs_model_ready, file = "/Volumes/George_Surgeon_Projects/MOC_vs_Outcome/data/medicare_abs_model_ready.rdata")
 
-medicare_abs_model_ready %>% 
-  select(ce_cert_status, flg_death_30d, flg_cmp_po_severe_not_poa, flg_readmit_30d,
-         flg_util_reop,emergent_admit, flg_assistant_surgeon, flg_hosp_urban) %>% 
-  mutate(flg_cmp_po_severe_not_poa = as.numeric(flg_cmp_po_severe_not_poa)) %>% 
-  gtsummary::tbl_summary(by = ce_cert_status) 
+# medicare_abs_model_ready %>% 
+#   select(re_cert_status, flg_death_30d, flg_cmp_po_severe_not_poa, flg_readmit_30d,
+#          flg_util_reop,emergent_admit, flg_assistant_surgeon, flg_hosp_urban) %>% 
+#   mutate(flg_cmp_po_severe_not_poa = as.numeric(flg_cmp_po_severe_not_poa)) %>% 
+#   gtsummary::tbl_summary(by = re_cert_status) 
 
 
 # model -------------------------------------------------------------------
 # sample data
-set.seed(123)
-sample_surg = sample_frac(abs, 0.1) %>% pull(npi)
-medicare_abs = medicare_abs %>% filter(npi %in% sample_surg)
+# set.seed(123)
+# sample_surg = sample_frac(abs, 0.1) %>% pull(npi)
+# medicare_abs = medicare_abs %>% filter(npi %in% sample_surg)
 
-
-
-medicare_abs_model_ready %>% count(ce_cert_status)
+medicare_abs_model_ready %>% count(re_cert_status)
 
 covariates = c(
-  'ce_cert_status',
+  # cert status
+  # choose one
+  # 're_cert_status',
+  're_cert_bin',
+  # other
   'flg_male',
   'age_at_admit_std',
   'AHRQ_score_std',
@@ -68,12 +77,24 @@ all(covariates %in% names(medicare_abs_model_ready))
 # lme4 --------------------------------------------------------------------
 
 f = formula(paste("flg_death_30d ~ 1", paste(covariates, collapse = ' + '),
-                  "(1 | id_physician_npi)", "(1 | e_proc_grp_lbl)",
+                   "(1 | e_proc_grp_lbl)",
                   sep = " + "))
 
 death_model = glmer(formula = f,
       data = medicare_abs_model_ready,
       family = binomial)
+
+save(death_model, file = "/Volumes/George_Surgeon_Projects/MOC_vs_Outcome/model/death_model.rdata")
+
+
+medicare_abs_model_ready_10yr_after_cert = medicare_abs_model_ready %>% 
+  filter(facility_clm_yr - Gcertyear >10)
+
+death_model_10yr = glmer(formula = f,
+                         data = medicare_abs_model_ready_10yr_after_cert,
+                         family = binomial)
+
+save(death_model_10yr, file = "/Volumes/George_Surgeon_Projects/MOC_vs_Outcome/model/death_model_10yr.rdata")
 
 # bayes -------------------------------------------------------------------
 
