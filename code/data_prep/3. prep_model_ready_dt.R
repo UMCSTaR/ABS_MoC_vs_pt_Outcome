e292986b65ee62c525eac5d56cdb81b1e3e2b442
Xilin Chen <xilinc@med.umich.edu>
2021-08-12 13:53:36 -0400

    update diagram to exclude incomplete cases

--- code/data_prep/3. prep_model_ready_dt.R
+++ code/data_prep/3. prep_model_ready_dt.R
@@ -11,5 +11,8 @@ load("/Volumes/George_Surgeon_Projects/MOC_vs_Outcome/data/abs_medicare_10_20yr_
 # debugonce(prep_model_ready_dt)
 medicare_abs_model_ready_no_na = prep_model_ready_dt(data = abs_medicare_union)
 
+nrow(medicare_abs_model_ready_no_na) # 710566
+n_distinct(medicare_abs_model_ready_no_na$npi) # 7397
+
 
 save(medicare_abs_model_ready_no_na, file = "/Volumes/George_Surgeon_Projects/MOC_vs_Outcome/data/medicare_abs_model_ready_no_na.rdata")
